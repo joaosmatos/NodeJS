881855c7a8b682fcf87b85d24915a768c20be167
joaosmatos <joaovsmatoss@gmail.com>
2025-12-03 00:12:20 -0300

    pegando dados do formulario

--- handlebars.js
+++ handlebars.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
-const Sequelize = require('sequelize')
+const Post = require('./models/post')
 
 
 // confg
@@ -12,17 +12,26 @@ const Sequelize = require('sequelize')
 // body parser
         app.use(bodyParser.urlencoded({extend: false}))
         app.use(bodyParser.json())        
-// conexão com o banco de dados MYSQL
 
-const Sequelize = require("sequelize")
-const sequelize = new Sequelize('test', 'root', 'saopaulofc1935@', {
-    host: "lolcalhost",
-    dialect: 'mysql'
-})
 // rotas
+
+    app.get('/', function(req, res){
+        res.render('home')
+    })
 
     app.get('/cad', function(req, res){
         res.render('formulario')
+    })
+
+    app.post('/add', function(req, res){
+        Post.create({
+            titulo: req.body.titulo,
+            conteudo: req.body.conteudo
+        }).then(function(){
+            res.redirect('/')
+        }).catch(function(erro){
+            res.send("houve um erro: " + erro)
+        })
     })
 
     app.post('/add',function(req, res){
